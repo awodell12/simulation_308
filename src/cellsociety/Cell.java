30f16294d019bf2e662e5a40984ddb0ac020decf
Austin Odell <awo6@duke.edu>
2020-01-31 19:23:03 -0500

    Created Cell class

---
+++ src/cellsociety/Cell.java
@@ -0,0 +1,16 @@
+package cellsociety;
+
+public class Cell {
+  int myType;
+  public Cell(int type){
+    myType = type;
+  }
+
+  public int getMyType() {
+    return myType;
+  }
+
+  public void setMyType(int type) {
+    this.myType = type;
+  }
+}
