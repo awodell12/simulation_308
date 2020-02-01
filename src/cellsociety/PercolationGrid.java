2d43019b2fb119525e01e3d777a9a49a71b0e2a5
Austin Odell <awo6@duke.edu>
2020-02-01 14:15:09 -0500

    added checkForUpdate and checkNeighbors methods

--- src/cellsociety/PercolationGrid.java
+++ src/cellsociety/PercolationGrid.java
@@ -8,6 +8,9 @@ import java.util.List;
  */
 public class PercolationGrid extends Grid {
 
+  public static int FILLED = 2;
+  public static int OPEN = 1;
+  public static int BLOCKED = 0;
 
   public PercolationGrid(int cols, int rows) {
     super(cols, rows);
@@ -19,11 +22,11 @@ public class PercolationGrid extends Grid {
     for (int i = 0; i < numRows ; i ++){
       for (int j =0; j>numColumns; j++){
         Cell curCell = myCellGrid[i][j];
-        if (curCell.getMyType()!=1){
+        if (curCell.getMyType()!=OPEN){
           continue;
         }
         int newType = checkNeighbors(i,j);
-        if (newType != 1)
+        if (newType != OPEN)
           updateList.add(new Cell(newType, i, j));
       }
     }
@@ -31,6 +34,31 @@ public class PercolationGrid extends Grid {
   }
 
   private int checkNeighbors(int i, int j) {
-    return 0;
+    int count =0;
+    boolean isTopEdge = (i == 0); //&& j!=0 && j!=numColumns);
+    boolean isBottomEdge = (i == numRows);// && j!=0 && j!=numColumns);
+    boolean isLeftEdge = (j == 0); // && i!= numRows && i != 0);
+    boolean isRightEdge = (j == numColumns); // && i!= numRows && i != 0);
+
+      if (!isLeftEdge && !isTopEdge) if (myCellGrid[i-1][j-1].getMyType() ==2) count ++;
+
+      if (!isTopEdge) if (myCellGrid[i-1][j].getMyType()==2) count ++;
+
+      if(!isTopEdge && !isRightEdge) if (myCellGrid[i-1][j+1].getMyType()==2) count ++;
+
+      if (!isLeftEdge) if (myCellGrid[i][j-1].getMyType()==2) count ++;
+
+      if(!isRightEdge) if (myCellGrid[i][j+1].getMyType()==2) count ++;
+
+      if(!isBottomEdge && !isLeftEdge) if (myCellGrid[i+1][j-1].getMyType()==2) count ++;
+
+      if(!isBottomEdge) if (myCellGrid[i+1][j].getMyType()==2) count ++;
+
+      if(!isBottomEdge && !isRightEdge) if (myCellGrid[i+1][j+1].getMyType()==2) count ++;
+
+    if (count > 0)
+    return FILLED;
+    return 1;
   }
+
 }
