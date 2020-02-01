f9fab477864f8222d63c294852402a0f29ab5da4
Austin Odell <awo6@duke.edu>
2020-02-01 14:53:09 -0500

    Made a small main class to test Percolation

--- src/cellsociety/Main.java
+++ src/cellsociety/Main.java
@@ -1,5 +1,8 @@
 package cellsociety;
 
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Feel free to completely change this code or delete it entirely. 
  */
@@ -7,8 +10,15 @@ public class Main {
     /**
      * Start of the program.
      */
-    public static void main (String[] args) {
-        System.out.println("Hello world");
+    public static void main (String[] args) throws InterruptedException {
+      Grid myGrid = new PercolationGrid(4,4);
+      ArrayList<Cell> arr = new ArrayList<>();
+      arr.add(new Cell(2,0,0));
+      myGrid.updateCells(arr);
+        for (int i = 0; i < 5; i++){
+          myGrid.updateCells(myGrid.checkForUpdates());
+          myGrid.printCells();
+      }
     }
 }
 
