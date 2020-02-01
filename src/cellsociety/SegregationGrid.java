b703bd334a91aa26c3c7c2028113dbc78457234c
Austin Odell <awo6@duke.edu>
2020-02-01 17:15:25 -0500

    Created segregation grid class

--- src/cellsociety/SegregationGrid.java
+++ src/cellsociety/SegregationGrid.java
@@ -1,5 +1,15 @@
 package cellsociety;
 
-public class SegregationGrid {
+import java.util.List;
 
+public class SegregationGrid extends Grid {
+
+  public SegregationGrid(int cols, int rows) {
+    super(cols, rows);
+  }
+
+  @Override
+  public List<Cell> checkForUpdates() {
+    return null;
+  }
 }
