76ab0e85f603d5c95c6e9179d65f4a1e976f2c14
Robert Chen <rec43@duke.edu>
2020-02-01 06:37:07 -0500

    Exceptions for XML parser

---
+++ src/xml/XMLException.java
@@ -0,0 +1,24 @@
+package xml;
+
+public class XMLException extends RuntimeException {
+    /**
+     * Create an exception based on an issue in our code.
+     */
+    public XMLException (String message, Object ... values) {
+        super(String.format(message, values));
+    }
+
+    /**
+     * Create an exception based on a caught exception with a different message.
+     */
+    public XMLException (Throwable cause, String message, Object ... values) {
+        super(String.format(message, values), cause);
+    }
+
+    /**
+     * Create an exception based on a caught exception, with no additional message.
+     */
+    public XMLException (Throwable cause) {
+        super(cause);
+    }
+}
