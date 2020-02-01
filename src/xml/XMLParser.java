a8a56300cff8e870a71aa2e69339f36aff920022
Robert Chen <rec43@duke.edu>
2020-02-01 06:41:46 -0500

    boilerplate parser function

---
+++ src/xml/XMLParser.java
@@ -0,0 +1,54 @@
+package xml;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class XMLParser {
+    private final DocumentBuilder DOCUMENT_BUILDER;
+    // private final String TYPE_ATTRIBUTE;
+
+    public XMLParser (String type) {
+        DOCUMENT_BUILDER = getDocumentBuilder();
+        // TYPE_ATTRIBUTE = type;
+    }
+
+    private Element getRootElement (File xmlFile) {
+        try {
+            DOCUMENT_BUILDER.reset();
+            Document xmlDocument = DOCUMENT_BUILDER.parse(xmlFile);
+            return xmlDocument.getDocumentElement();
+        }
+        catch (SAXException | IOException e) {
+            throw new XMLException(e);
+        }
+    }
+
+    private String getTextValue (Element e, String tagName) {
+        NodeList nodeList = e.getElementsByTagName(tagName);
+        if (nodeList != null && nodeList.getLength() > 0) {
+            return nodeList.item(0).getTextContent();
+        }
+        else {
+            // FIXME: Possibly fix because in some cases it may be an error to not find any text
+            return "";
+        }
+    }
+
+
+    private DocumentBuilder getDocumentBuilder () {
+        try {
+            return DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        }
+        catch (ParserConfigurationException e) {
+            throw new XMLException(e);
+        }
+    }
+
+}
