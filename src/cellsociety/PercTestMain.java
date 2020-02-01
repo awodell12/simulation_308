94923c1b0a5aa6c3e0749ef7a8334bc9dadde709
Austin Odell <awo6@duke.edu>
2020-02-01 16:44:21 -0500

    Added Main class to test perc

--- src/cellsociety/PercTestMain.java
+++ src/cellsociety/PercTestMain.java
@@ -1,19 +1,21 @@
 package cellsociety;
 
 import java.util.ArrayList;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Feel free to completely change this code or delete it entirely. 
  */
-public class Main {
+public class PercTestMain {
     /**
      * Start of the program.
      */
-    public static void main (String[] args) throws InterruptedException {
+    public static void main (String[] args) {
       Grid myGrid = new PercolationGrid(4,4);
       ArrayList<Cell> arr = new ArrayList<>();
       arr.add(new Cell(2,0,0));
+      arr.add(new Cell (1,0,1));
+      arr.add(new Cell(0, 1,1));
+      arr.add(new Cell (1,1,0));
       myGrid.updateCells(arr);
         for (int i = 0; i < 5; i++){
           myGrid.updateCells(myGrid.checkForUpdates());
