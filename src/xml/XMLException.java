c4f296e936417cc8c325fee1e4c0f31a2242726a
Robert Chen <rec43@duke.edu>
2020-02-02 05:55:46 -0500

    remove unnecessary exception case

--- src/xml/XMLException.java
+++ src/xml/XMLException.java
@@ -9,13 +9,6 @@ public class XMLException extends RuntimeException {
     }
 
     /**
-     * Create an exception based on a caught exception with a different message.
-     */
-    public XMLException (Throwable cause, String message, Object ... values) {
-        super(String.format(message, values), cause);
-    }
-
-    /**
      * Create an exception based on a caught exception, with no additional message.
      */
     public XMLException (Throwable cause) {
