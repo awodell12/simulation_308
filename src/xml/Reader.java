613071bec0f283d053290a628a3b70cf9bd80e72
Robert Chen <rec43@duke.edu>
2020-02-02 20:31:38 -0500

    select xml file with FileChooser

--- src/xml/Reader.java
+++ src/xml/Reader.java
@@ -1,6 +1,6 @@
 package xml;
 
-/*
+import java.io.File;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.scene.control.Alert;
@@ -8,44 +8,30 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.stage.FileChooser.ExtensionFilter;
- */
 
-import java.io.File;
 import org.w3c.dom.Element;
 
-public class Reader {
+public class Reader extends Application {
 
-    public static final String GAME_OF_LIFE = "data/GameOfLife.xml";
-    private static File dataFile = new File(GAME_OF_LIFE);
+    public static final String DATA_FILE_EXTENSION = "*.xml";
+    public final static FileChooser FILE_CHOOSER = makeChooser(DATA_FILE_EXTENSION);
 
-    public static void main(String[] args) {
-        XMLParser myParser = new XMLParser();
-        Element e = myParser.getRootElement(dataFile);
-        String output = myParser.getTextValue(e,"config");
-        System.out.print(output);
-    }
+    private Configuration config;
 
-
-    // public static final String DATA_FILE_EXTENSION = "*.xml";
-    // public final static FileChooser FILE_CHOOSER = makeChooser(DATA_FILE_EXTENSION);
-
-    /*
     @Override
     public void start (Stage primaryStage) throws Exception {
         File dataFile = FILE_CHOOSER.showOpenDialog(primaryStage);
         while (dataFile != null) {
             try {
                 XMLParser myParser = new XMLParser();
-                Element e = myParser.getRootElement(dataFile);
-                String output = myParser.getTextValue(e,"config");
-                System.out.print(output);
-
+                config = myParser.getConfiguration(dataFile);
+                System.out.print(config.getHeight());
             }
             catch (XMLException e) {
                 // handle error of unexpected file format
                 showMessage(AlertType.ERROR, e.getMessage());
             }
-            dataFile = FILE_CHOOSER.showOpenDialog(primaryStage);
+            break;
         }
         // nothing selected, so quit the application
         Platform.exit();
@@ -55,7 +41,6 @@ public class Reader {
         new Alert(type, message).showAndWait();
     }
 
-    // Choose file manually, like in Spike Lab
     private static FileChooser makeChooser (String extensionAccepted) {
         FileChooser result = new FileChooser();
         result.setTitle("Open Data File");
@@ -68,5 +53,5 @@ public class Reader {
     public static void main(String[] args) {
         launch(args);
     }
-     */
+
 }
