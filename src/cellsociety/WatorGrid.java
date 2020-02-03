d0bbcdb00d2a19c1cdcd74d1ae9d6a024699deac
Austin Odell <awo6@duke.edu>
2020-02-03 13:14:56 -0500

    Created Wa-Tor Grid

--- src/cellsociety/WatorGrid.java
+++ src/cellsociety/WatorGrid.java
@@ -1,5 +1,48 @@
 package cellsociety;
 
-public class WatorGrid {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+import javafx.util.Pair;
 
+@SuppressWarnings("SpellCheckingInspection")
+public class WatorGrid extends Grid {
+
+  public static final int SHARK = 2;
+  public static final int FISH = 1;
+  public static final int EMPTY = 0;
+
+  private int fishTimeToBreed;
+  private int sharkTimeToBreed;
+  private int sharkDeathTime;
+
+  private List myFish;
+  private List mySharks;
+
+
+
+  public WatorGrid(int cols, int rows, int sharkBreed, int fishBreed, int sharkDie ) {
+    super(cols, rows);
+    fishTimeToBreed = fishBreed;
+    sharkDeathTime = sharkDie;
+    sharkTimeToBreed = sharkBreed;
+  }
+
+  @Override
+  public List<Cell> checkForUpdates() {
+    ArrayList<Cell> updateList = new ArrayList<>();
+    ArrayList<Pair> emptyCells = new ArrayList<>();
+    emptyCells = findEmptyCells();
+    for (int i = 0; i < numRows; i++) {
+      for (int j = 0; j < numColumns; j++) {
+        Cell curCell = myCellGrid[i][j];
+
+        if (curCell.myType==FISH){
+
+        }
+      }
+    }
+    return updateList;
+  }
 }
+
