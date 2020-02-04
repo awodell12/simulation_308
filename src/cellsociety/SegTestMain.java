542fd69c13330876df3f2a3e48e9b90760d3a003
Austin Odell <awo6@duke.edu>
2020-02-03 21:08:09 -0500

    Updated to test Pred-Prey

--- src/cellsociety/SegTestMain.java
+++ src/cellsociety/SegTestMain.java
@@ -11,22 +11,19 @@ public class SegTestMain {
    * Start of the program.
    */
   public static void main(String[] args) {
-    Grid myGrid = new SegregationGrid(3, 5, 0.5);
+    Grid myGrid = new WatorGrid(5, 5, 5, 5, 2);
     ArrayList<Cell> arr = new ArrayList<>();
     arr.add(new Cell(1, 0, 0));
     arr.add(new Cell(1, 0, 2));
     arr.add(new Cell(1, 2, 2));
     arr.add(new Cell(1, 2, 0));
-    arr.add(new Cell(1, 3, 2));
     arr.add(new Cell(1, 4, 2));
     arr.add(new Cell(2, 1, 0));
-    arr.add(new Cell(2, 1, 2));
-    arr.add(new Cell(2, 2, 0));
     arr.add(new Cell(2, 3, 2));
 
     myGrid.updateCells(arr);
     myGrid.printCells();
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 10; i++) {
       myGrid.updateCells(myGrid.checkForUpdates());
       myGrid.printCells();
     }
